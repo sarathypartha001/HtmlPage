2ba69f2de4a03065a3f3f8a606dae220284a036f
sarathypartha001 <parthasarathya001@gmail.com>
2025-02-28 20:35:59 +0530

    modified files

--- about.js
+++ about.js
@@ -11,3 +11,54 @@ sidenavclosebar.addEventListener("click", function(){
 })
 // header session end
 
+// about session
+
+
+const images = [
+    "images/moonlit5.jpeg",
+    "images/summarpalace.jpeg",
+    "images/silverspring.jpg",
+    "images/Rainbow\ Cottage\ 3.jpg",
+    "images/moonlit6.jpeg",
+]
+
+let currentdataContainer = 0;
+let aboutHomeImg  = document.querySelector(".about-home-image");
+let aboutLeftArow =  document.querySelector(".left-arow");
+let aboutRightArow = document.querySelector(".right-arow");
+let aboutRadioBtn =  document.querySelectorAll(".about-radio-input");
+
+function changeAboutImg(index){
+    currentdataContainer = index;
+    aboutHomeImg.style.backgroundImage = `url('${images[currentdataContainer]}')`;
+    aboutRadioBtn[currentdataContainer].checked = true;
+}
+
+function AutoSlide(){
+    currentdataContainer = (currentdataContainer+1)%images.length
+    changeAboutImg(currentdataContainer)
+}
+
+let aboutSlideInterval = setInterval(AutoSlide, 3000);
+
+aboutRadioBtn.forEach((radioBtn, index)=>{
+       radioBtn.addEventListener("click",()=>{
+          clearInterval(aboutSlideInterval);
+          changeAboutImg(index);
+          aboutSlideInterval = setInterval(AutoSlide, 3000);
+       })
+})
+
+aboutLeftArow.addEventListener("click", function(){
+    clearInterval(aboutSlideInterval);
+    currentdataContainer = (currentdataContainer - 1+images.length)%images.length
+    changeAboutImg(currentdataContainer);
+    aboutSlideInterval = setInterval(AutoSlide, 3000);
+})
+aboutRightArow.addEventListener("click", ()=>{
+    clearInterval(aboutSlideInterval);
+    currentdataContainer = (currentdataContainer + 1)%images.length
+    changeAboutImg(currentdataContainer)
+    aboutSlideInterval = setInterval(AutoSlide, 3000)
+})
+// about session end
